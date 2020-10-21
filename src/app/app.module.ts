b54914ef5f23c0fa4ba56f2638c7e0e39add85ae
César Gonzalez <cedgo1997@gmail.com>
2020-10-21 12:38:30 -0400

    todo template and todo module created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { TodoModule } from "src/app/todos/todo.module";
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TodoModule
   ],
   providers: [],
   bootstrap: [AppComponent]
