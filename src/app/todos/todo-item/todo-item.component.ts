f842457b5a0704ca372ec14c7e02f15505061932
César Gonzalez <cedgo1997@gmail.com>
2020-10-21 22:10:21 -0400

    Displaying list items

--- src/app/todos/todo-item/todo-item.component.ts
+++ src/app/todos/todo-item/todo-item.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { Todo } from '../models/todo.model';
 
 @Component({
   selector: 'app-todo-item',
   templateUrl: './todo-item.component.html',
-  styleUrls: ['./todo-item.component.css']
+  styleUrls: ['./todo-item.component.css'],
 })
 export class TodoItemComponent implements OnInit {
+  @Input() todo: Todo;
+  @ViewChild('fisicTxtInput') fisicTxtInput: ElementRef;
 
-  constructor() { }
+  chkCompleted: FormControl;
+  txtInput: FormControl;
+
+  editing: boolean = false;
+
+  constructor() {}
 
   ngOnInit(): void {
+    this.chkCompleted = new FormControl(this.todo.completed);
+    this.txtInput = new FormControl(this.todo.text, Validators.required);
   }
 
+  edit() {
+    this.editing = true;
+
+    setTimeout(() => {
+      this.fisicTxtInput.nativeElement.select();
+    }, 1);
+  }
+
+  endEdit() {
+    this.editing = false;
+  }
 }
--- src/app/todos/todo.reducer.ts
+++ src/app/todos/todo.reducer.ts
@@ -2,7 +2,14 @@ import { createReducer, on } from '@ngrx/store';
 import { create } from './todo.actions';
 import { Todo } from './models/todo.model';
 
-export const initialState: Todo[] = [];
+export const initialState: Todo[] = [
+
+  new Todo('Save the world'),
+  new Todo('Defeat Thanos'),
+  new Todo('Build another ironman suit')
+
+
+];
 
 const _todoReducer = createReducer(
   initialState,
